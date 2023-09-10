e909cd140980ff3ca4052dcc3b7f32f59db8dda7
Georgii <nosleep2night@yandex.ru>
2023-09-11 02:33:08 +0400

    ТЗ-16

--- gateway/src/main/java/ru/practicum/shareit/item/dto/CommentDto.java
+++ gateway/src/main/java/ru/practicum/shareit/item/dto/CommentDto.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.item.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import ru.practicum.shareit.validation.Create;
 
 import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
